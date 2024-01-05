f2f1c824d0dafe4efceebf84b9b9d8946a3edbbc
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-01-05 18:46:28 +0900

    実装

--- dalleShortName/Program.cs
+++ dalleShortName/Program.cs
@@ -1,9 +1,91 @@
-﻿namespace dalleShortName
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+
+namespace dalleShortName
 {
     internal class Program
     {
+        // A simple program to simplify file names by DALL-E.
+
+        // Example:
+        // From: DALL·E 2024-01-05 15.44.53 - A whimsical and surreal wide-format illustration of snowy Moscow, featuring bears drinking vodka. The scene is set in a snowy landscape with iconic Mo.png
+        // To: DALL-E 2024-01-05 15.44.53.png
+
+        // It also generates the JPEG version of the image.
+
         static void Main (string [] args)
         {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine ("Drag & drop the files to rename to the app's executable file.");
+                    return;
+                }
+
+                foreach (string xFilePath in args)
+                {
+                    if (Path.IsPathFullyQualified (xFilePath) == false || File.Exists (xFilePath) == false)
+                    {
+                        Console.WriteLine ("Invalid file path: " + xFilePath);
+                        continue;
+                    }
+
+                    string xFileNameWithoutExtension = Path.GetFileNameWithoutExtension (xFilePath);
+
+                    Match xMatch = Regex.Match (xFileNameWithoutExtension, @"^(?<FirstPart>DALL·E [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}\.[0-9]{2}\.[0-9]{2}).+?$",
+                        RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+                    if (xMatch.Success == false)
+                    {
+                        Console.WriteLine ("Invalid file name: " + Path.GetFileName (xFilePath));
+                        continue;
+                    }
+
+                    string xFirstPart = xMatch.Groups ["FirstPart"].Value.Replace ('·', '-'),
+                        xNewFileName = xFirstPart + Path.GetExtension (xFilePath),
+                        xNewFilePath = Path.Join (Path.GetDirectoryName (xFilePath), xNewFileName);
+
+                    try
+                    {
+                        File.Move (xFilePath, xNewFilePath);
+                        Console.WriteLine ("Renamed to: " + xNewFileName);
+                    }
+
+                    catch
+                    {
+                        Console.WriteLine ("Failed to rename: " + Path.GetFileName (xFilePath));
+                        continue;
+                    }
+
+                    try
+                    {
+                        using Image xImage = Image.FromFile (xNewFilePath);
+                        xImage.Save (Path.ChangeExtension (xNewFilePath, ".jpg"), ImageFormat.Jpeg); // Default settings.
+                        Console.WriteLine ("Generated JPEG version: " + Path.ChangeExtension (xNewFileName, ".jpg"));
+                    }
+
+                    catch
+                    {
+                        Console.WriteLine ("Failed to generate JPEG version: " + Path.ChangeExtension (xNewFileName, ".jpg"));
+                        continue;
+                    }
+                }
+            }
+
+            catch (Exception xException)
+            {
+                // Trimming the following line-break if there is one.
+                Console.WriteLine (xException.ToString ().TrimEnd ());
+            }
+
+            finally
+            {
+                Console.Write ("Press any key to close this window: ");
+                Console.ReadKey (true);
+                Console.WriteLine ();
+            }
         }
     }
 }
