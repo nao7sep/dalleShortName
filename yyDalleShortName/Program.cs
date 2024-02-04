acfcdafc542570d127a5ab3439602b5ff25ee6b8
Yoshinao Inoguchi <apple@inoguchi.com>
2024-02-04 14:43:02 +0900

    Updated code to new project name

--- yyDalleShortName/Program.cs
+++ yyDalleShortName/Program.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace dalleShortName
+namespace yyDalleShortName
 {
     internal partial class Program
     {
