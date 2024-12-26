20c52220ff97ee1f25aa1c489f2bc441f3870733
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-12-26 10:39:06 +0900

    Can build

--- dalleShortName/Program.cs
+++ dalleShortName/Program.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace yyDalleShortName
+namespace dalleShortName
 {
     internal partial class Program
     {
